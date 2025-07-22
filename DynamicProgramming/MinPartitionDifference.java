04f2dd2e9d7c79653e5768a8be3a51cba711a651
100RAV22 <saurav.kumar310704@gmail.com>
2025-07-22 23:30:52 +0530

    0_1 knapsack variation problem , minimum partition

---
+++ DynamicProgramming/MinPartitionDifference.java
@@ -0,0 +1,30 @@
+package DynamicProgramming;
+import java.util.*;
+public class MinPartitionDifference {
+    public static int minPartitionDifference(int[] arr) {
+        int n = arr.length;
+        int sum = 0;
+        for(int a : arr){
+            sum += a;
+        }
+        int W = sum/2;
+        int dp[][] = new int[n+1][W+1];
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j] = Math.max(arr[i-1]+dp[i-1][j-arr[i-1]], dp[i-1][j]);
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        int sum1 = dp[n][W];
+        int sum2 = sum-sum1;
+        return Math.abs(sum1-sum2);
+    }
+    public static void main(String args[]){
+        int arr[] = {2,3,1,1,4};
+        int res = minPartitionDifference(arr);
+        System.out.println("minimum difference : "+res);
+    }
+}
